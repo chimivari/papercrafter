73900758e284500c5e9d397c0f3ec937637973aa
andeol_vella <chimivari@gmail.com>
2025-01-31 22:36:55 +0100

    Get triangles through dll and array of usize

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,46 @@
-use std::ffi::{c_char, CString};
+use std::{ffi::{c_char, CString}, fmt::Display};
+
+struct Vertex {
+    x: f64,
+    y: f64,
+    z: f64,
+}
+impl Vertex {
+    fn new(x: f64, y: f64, z: f64) -> Self {
+        Self {x, y, z}
+    }
+}
+impl Display for Vertex {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({}, {}, {})", self.x, self.y, self.z)
+    }
+}
+
+struct Triangle {
+    a: usize,
+    b: usize,
+    c: usize,
+}
+impl Triangle {
+    fn new(a: usize, b: usize, c: usize) -> Self {
+        Self {a, b, c}
+    }
+}
+impl Display for Triangle {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({}, {}, {})", self.a, self.b, self.c)
+    }
+}
 
 #[link(name = "MeshSimplifier", kind = "dylib")]
 extern {
     fn simplify(file_path: *const c_char, export_path: *const c_char, reduce_fraction: f32, agressiveness: f32) -> i32;
+
     fn get_vertices(count: *mut usize) -> *mut f64;
     fn free_vertices(ptr: *mut f64);
+
+    fn get_triangles(count: *mut usize) -> *mut usize;
+    fn free_triangles(ptr: *mut usize);
 }
 
 fn main() {
@@ -24,9 +60,37 @@ fn main() {
         if ptr.is_null() {
             eprintln!("Returned ptr is null");
         }
-
-        let slice = std::slice::from_raw_parts(ptr, size);
-        println!("{:?}", slice);
+        let vertices_slice = std::slice::from_raw_parts(ptr, size);
+        let mut vertices = Vec::new();
+        let mut i = 0;
+        while i < size {
+            let vertex = Vertex::new(
+                vertices_slice[i], 
+                vertices_slice[i + 1], 
+                vertices_slice[i + 2],
+            );
+            vertices.push(vertex);
+            i += 3;
+        }
         free_vertices(ptr);
+
+        let mut size = 0;
+        let ptr = get_triangles(&mut size);
+        if ptr.is_null() {
+            eprintln!("Returned ptr is null");
+        }
+        let triangles_slice = std::slice::from_raw_parts(ptr, size);
+        let mut triangles = Vec::new();
+        let mut i = 0;
+        while i < size {
+            let triangle = Triangle::new(
+                triangles_slice[i], 
+                triangles_slice[i + 1], 
+                triangles_slice[i + 2],
+            );
+            triangles.push(triangle);
+            i += 3;
+        }
+        free_triangles(ptr);
     }
 }
